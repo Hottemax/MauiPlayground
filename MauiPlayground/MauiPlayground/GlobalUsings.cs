0962e8036b89dd648dc2917f89bc27cab1c6a65a
Stephan Kieburg <s.kieburg@jpsc.de>
2023-02-06 12:04:01 +0100

    [IGNORE] GlobalUsings

---
+++ MauiPlayground/MauiPlayground/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿// Contains global usings imported into all files in the project
+global using CommunityToolkit.Mvvm.Input;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using MauiPlayground.ViewModels;
+global using MauiPlayground.Pages;
+global using MauiPlayground.Controls;
