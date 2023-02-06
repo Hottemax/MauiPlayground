c7e297236a359f7a37c97fa7759e83e985f09439
Stephan Kieburg <s.kieburg@jpsc.de>
2023-02-06 12:19:26 +0100

    [IGNORE] Ensure compilability with basic CI

--- MauiPlayground/MauiPlayground/GlobalUsings.cs
+++ MauiPlayground/MauiPlayground/GlobalUsings.cs
@@ -1,6 +1,8 @@
-﻿// Contains global usings imported into all files in the project
+﻿#pragma warning disable IDE0005 // Using directive is unnecessary.
+// Contains global usings imported into all files in the project
+
 global using CommunityToolkit.Mvvm.Input;
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using MauiPlayground.ViewModels;
 global using MauiPlayground.Pages;
-global using MauiPlayground.Controls;
+
