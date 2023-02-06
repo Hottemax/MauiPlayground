246d151693aea3d0f0364249c432834efbe51f9b
Stephan Kieburg <s.kieburg@jpsc.de>
2023-02-06 17:21:36 +0100

    WIP: DateAndTimePicker

--- MauiPlayground/MauiPlayground/ViewModels/DateAndTimeViewModel.cs
+++ MauiPlayground/MauiPlayground/ViewModels/DateAndTimeViewModel.cs
@@ -1,16 +1,18 @@
 ﻿namespace MauiPlayground.ViewModels;
 
-public partial class DateAndTimeViewModel :ObservableObject
+public partial class DateAndTimeViewModel : ObservableObject
 {
     [ObservableProperty]
     public DateTime? _sampleDate = DateTime.Now;
 
+    [ObservableProperty]
+    public TimeSpan? _sampleTime = new(hours: 3, minutes: 14, seconds: 0);
 
     [ObservableProperty]
     public string _testString = "Test";
 
     partial void OnSampleDateChanged(DateTime? value)
     {
-        Console.WriteLine($"SampleDate changed to {value}");
+        Log.Debug($"SampleDate changed to {value}");
     }
 }
